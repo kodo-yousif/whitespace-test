62db93f7125215d950690f3ad29b4207dca7ddfc
kodo yousif <kodo.00257248@gmail.com>
2021-12-22 20:45:59 +0300

    image conatiner added along with its css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from "./containers/Navigation";
 import Catagories from "./containers/Catagories";
+import Images from "./containers/Images";
 
 function App() {
   return (
     <>
       <Navigation />
       <Catagories />
+      <Images />
     </>
   );
 }
---
+++ src/containers/Images.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/Images.css";
+
+export default function Images() {
+  return <div id="images">images</div>;
+}
