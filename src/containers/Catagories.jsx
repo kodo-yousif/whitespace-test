b937383bceb592927f8bfb009c18451ca41a2970
kodo yousif <kodo.00257248@gmail.com>
2021-12-22 20:28:13 +0300

    second section done

--- src/containers/Catagories.jsx
+++ src/containers/Catagories.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "../styles/Catagories.css";
 
 export default function Catagories() {
-  return <div id="catagories">slaw</div>;
+  return (
+    <div id="catagories">
+      <button className="active">Photo & Videos</button>
+      <button>Map</button>
+      <button>360 View</button>
+    </div>
+  );
 }
