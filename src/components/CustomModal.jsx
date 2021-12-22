6fb0b08fe3f760517e2b7c55277a43d9f49718e8
kodo yousif <kodo.00257248@gmail.com>
2021-12-22 23:42:05 +0300

    header of modal removed

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Modal } from "antd";
+import "../styles/CustomModal.css";
 
 export default function CustomModal(props) {
   return (
-    <div id="custom">
-      <Modal
-        title="Basic Modal"
-        visible={props.switch}
-        onCancel={() => props.SetSwitch(false)}
-        footer={false}
-      >
-        {props.Selected.map((el) => (
-          <div>{el}</div>
-        ))}
-      </Modal>
-    </div>
+    <Modal
+      wrapClassName="customModal"
+      closable={false}
+      title="Basic Modal"
+      visible={props.switch}
+      onCancel={() => props.SetSwitch(false)}
+      footer={false}
+      width="90vw"
+    >
+      {props.Selected.map((el) => (
+        <div>{el}</div>
+      ))}
+    </Modal>
   );
 }
