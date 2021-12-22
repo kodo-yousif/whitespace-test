1f13f6b37e0990511d94517879dc2dec883a8f34
kodo yousif <kodo.00257248@gmail.com>
2021-12-22 20:10:39 +0300

    second container added with oits css file 'Catagories'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./containers/Navigation";
+import Catagories from "./containers/Catagories";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Catagories />
     </>
   );
 }
---
+++ src/containers/Catagories.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/Catagories.css";
+
+export default function Catagories() {
+  return <div id="catagories">slaw</div>;
+}
