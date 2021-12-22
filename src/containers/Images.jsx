58a63d8e9c69b427a9b4875fe00b2640ccda4171
kodo yousif <kodo.00257248@gmail.com>
2021-12-22 21:45:59 +0300

    basic image seaction is done

--- src/containers/Images.jsx
+++ src/containers/Images.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Images.css";
 import axios from "axios";
 
 export default function Images() {
+  const [images, setImages] = useState([]);
   useEffect(startUp, []);
 
   function startUp() {
-    console.log("slaw");
+    axios.get("https://picsum.photos/v2/list?page=2&limit=10").then((res) => {
+      console.log("done");
+      setImages(res.data);
+    });
   }
 
-  return <div id="images">images</div>;
+  return (
+    <div style={{ paddingBottom: 20 }}>
+      <ul id="images">
+        {images.map((image) => (
+          <li key={image.id}>
+            <img
+              loading="lazy"
+              src={image.download_url}
+              alt={`taken by author ${image.author}`}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
