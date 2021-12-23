a25bbc337573d0dfc836248145f2d84e1f1f1c77
kodo yousif <kodo.00257248@gmail.com>
2021-12-23 08:34:22 +0300

    heart icon nature changed to red onclick but share icon still on hover

--- src/components/NavigationRightIcons.jsx
+++ src/components/NavigationRightIcons.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShareIcon from "../assets/ShareIcon.svg";
 import RedShareIcon from "../assets/RedShareIcon.svg";
 import HeartIcon from "../assets/HeartIcon.svg";
@@ -6,6 +6,7 @@ import RedHeartIcon from "../assets/RedHeartIcon.svg";
 import Close from "../assets/Close.svg";
 
 export default function NavigationRightIcons(props) {
+  const [isRed, setChange] = useState(false);
   return (
     <div className="nav_right">
       <div className="icons scrollAnimateIcon">
@@ -13,7 +14,12 @@ export default function NavigationRightIcons(props) {
         <img className="alticon" src={RedShareIcon} alt="red share icon" />
         <span className="redHover">Share</span>
       </div>
-      <div className="icons scrollAnimateIcon">
+      <div
+        className={`icons clickAnimateIcon ${
+          isRed ? "clickedAnimateIcon" : ""
+        }`}
+        onClick={() => setChange(!isRed)}
+      >
         <img
           className="go_go_icon"
           src={HeartIcon}
@@ -21,7 +27,13 @@ export default function NavigationRightIcons(props) {
           alt="heart icon"
         />
         <img className="alticon" src={RedHeartIcon} alt="red heart icon" />
-        {props.additional ? <span className="redHover">Save</span> : ""}
+        {props.additional ? (
+          <span style={{ color: isRed ? "red" : "" }} className="redHover">
+            Save
+          </span>
+        ) : (
+          ""
+        )}
       </div>
       {props.additional && (
         <div className="icons" onClick={() => props.switch(false)}>
